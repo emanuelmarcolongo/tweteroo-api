8329c4a331ae48272da4a2ebd8a6809f324f921b
Emanuel Marcolongo <emanuelmarcolongo@pop-os.localdomain>
2022-11-04 13:26:41 -0300

    feat: users and tweets storaged in memory

--- index.js
+++ index.js
@@ -5,15 +5,28 @@ const app = express();
 app.use(cors());
 app.use(json());
 
+const users = [];
+const tweets = [];
 
+const userFormat = 
+{
+    username: 'cachorrineo',
+    avatar: "https://conteudo.imguol.com.br/c/entretenimento/eb/2022/03/23/cachorro-da-raca-lulu-da-pomeramia-1648065976007_v2_900x506.jpg"
+}
 
-app.get('/receitas', (req, res) => {
+const tweetFormat = 
+{
+    username: "bobesponja",
+  tweet: "eu amo o hub"
+}
+
+app.get('', (req, res) => {
     console.log(res)
 });
 
 
-app.post('/receitas', (req, res) => {
-    console.log(res)
+app.post('/sign-up', (req, res) => {
+   const {username, avatar} = req.body;
 })
 
 app.listen(5000);
