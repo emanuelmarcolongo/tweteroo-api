c2e3d0cc5cc6c1b52739a450235f0e06f83d5e99
Emanuel Marcolongo <emanuelmarcolongo@pop-os.localdomain>
2022-11-04 13:47:58 -0300

    feat: post and get tweets

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-const user = [];
+const currentUser = [];
 
 const users = [];
 
@@ -20,20 +20,27 @@ const tweets = [{
   tweet: "eu amo o hub"
 }];
 
-const userFormat = 
-{
-    username: 'cachorrineo',
-    avatar: "https://conteudo.imguol.com.br/c/entretenimento/eb/2022/03/23/cachorro-da-raca-lulu-da-pomeramia-1648065976007_v2_900x506.jpg"
-}
+app.get ("/tweets", (req, res) => {
 
-const tweetFormat = 
-{
-    username: "bobesponja",
-  tweet: "eu amo o hub"
-}
+    res.send(tweets);
+});
 
-app.get('/tweets', (req, res) => {
-    res.send(tweets)
+
+app.post('/tweets', (req, res) => {
+    const {username, tweet} = req.body;
+
+    const newTweet = {
+    username,
+    tweet
+    }
+
+    if (!username || !tweet) {
+        res.sendStatus(400).send("Preencha todos os campos");
+        return;
+       }
+
+    tweets.push(newTweet);
+    res.sendStatus(200).send("OK")
 });
 
 
@@ -58,8 +65,9 @@ app.post('/sign-up', (req, res) => {
     return;
    }
    
+   currentUser.push(newUser);
    users.push(newUser);
-   res.sendStatus(200);
+   res.sendStatus(200).send("OK");
 })
 
 app.listen(5000);
